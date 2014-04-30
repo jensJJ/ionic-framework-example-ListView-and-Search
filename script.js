b17284eaec4c65969ffde3bb1383337d6ccf3c6c
Jens <jens@jenss-mbp.fritz.box>
2014-04-30 04:42:23 +0200

    init2

---
+++ script.js
@@ -0,0 +1,12 @@
+// Code goes here
+
+angular.module('myapp', ['ionic'])
+
+.controller('FriendCtrl', function($scope) {
+  $scope.friends = [{name:'John', phone:'555-1276'},
+                         {name:'Mary', phone:'800-BIG-MARY'},
+                         {name:'Mike', phone:'555-4321'},
+                         {name:'Adam', phone:'555-5678'},
+                         {name:'Julie', phone:'555-8765'},
+                         {name:'Juliette', phone:'555-5678'}];
+});
